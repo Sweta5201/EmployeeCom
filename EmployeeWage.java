8375ae48a9d607e7ba828c5924590aae4a9a3968
Sweta5201 <sweta65959@gmail.com>
2020-09-26 14:22:41 +0530

    [sweta kumari] uc2 done

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,12 +1,15 @@
 class EmployeeWage{
 public static void main(String[] args){
 int is_present = 1;
+int rate_per_hour = 20;
+int emp_hrs = 0;
+int emp_wage = 0;
 int check =(int) Math.floor(Math.random() *10) % 2;
-if(check == is_present){
-System.out.println("Employee is present");
-}
-else{
-System.out.println("Employee is absent");
-}
+if(check == is_present)
+emp_hrs = 8;
+else
+emp_hrs = 0;
+emp_wage = emp_hrs * rate_per_hour;
+System.out.println("Employee wage is "+emp_wage);
 }
 }
