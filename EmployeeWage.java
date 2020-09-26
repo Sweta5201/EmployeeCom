8847b6bfdd210236abfd1561259ca98b5c720bf4
Sweta5201 <sweta65959@gmail.com>
2020-09-26 14:25:35 +0530

    [sweta kumari] uc3 done

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,15 +1,18 @@
 class EmployeeWage{
 public static void main(String[] args){
-int is_present = 1;
-int rate_per_hour = 20;
-int emp_hrs = 0;
-int emp_wage = 0;
-int check =(int) Math.floor(Math.random() *10) % 2;
-if(check == is_present)
-emp_hrs = 8;
+int Part_time = 1;
+int Full_time =2;
+int Rate_per_hour = 20;
+int Emp_hrs = 0;
+int Emp_wage = 0;
+int check =(int) Math.floor(Math.random() *10) % 3;
+if(check == Part_time)
+Emp_hrs = 4;
+else if(check == Full_time)
+Emp_hrs = 8;
 else
-emp_hrs = 0;
-emp_wage = emp_hrs * rate_per_hour;
-System.out.println("Employee wage is "+emp_wage);
+Emp_hrs = 0;
+Emp_wage = Emp_hrs * Rate_per_hour;
+System.out.println("Employee wage is "+Emp_wage);
 }
 }
