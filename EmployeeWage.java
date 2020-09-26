bff7ceb9e8687e12f4bd69985455b08f34e7aa9c
Sweta5201 <sweta65959@gmail.com>
2020-09-26 14:33:32 +0530

    [sweta kumari] uc6 done

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,12 +3,16 @@ public static final int Part_time = 1;
 public static final int Full_time =2;
 public static final int Rate_per_hour = 20;
 public static final int Working_days = 20;
+public static final int max_hrs_in_month = 100;
 public static void main(String[] args){
 int Emp_hrs = 0;
 int Emp_wage = 0;
 int Total_wage = 0;
-for(int day=0; day<Working_days;day++)
+int totalhrs = 0;
+int totalworkingdays=0;
+while(totalhrs <= max_hrs_in_month && totalworkingdays < Working_days)
 {
+totalworkingdays++;
 int check =(int) Math.floor(Math.random() *10) % 3;
 switch(check){
 case Part_time: Emp_hrs = 4;
@@ -17,10 +21,10 @@ case Full_time: Emp_hrs = 8;
                 break;
 default: Emp_hrs = 0;
 }
-Emp_wage = Emp_hrs * Rate_per_hour;
-Total_wage+=Emp_wage;
+totalhrs+=Emp_hrs;
+System.out.println("Days : "+totalworkingdays +" Hours : "+Emp_hrs);
 }
+Total_wage = totalhrs * Rate_per_hour;
 System.out.println("Total Employee wage is "+Total_wage);
 }
 }
-
