42d38ca256a8b42c26b207831e76629296557b76
Sweta5201 <sweta65959@gmail.com>
2020-09-29 17:17:57 +0530

    [sweta kumari] uc10 done

---
+++ CompanyEmpWage.java
@@ -0,0 +1,24 @@
+public class CompanyEmpWage {
+
+		public final String company;
+		public final int empRatePerHr;
+		public final int noOfDays;
+		public final int maxHrsPerMonth;
+		public int totalWage;
+
+		public CompanyEmpWage(String company, int empRatePerHr, int noOfDays, int maxHrsPerMonth){
+			this.company = company;
+			this.empRatePerHr = empRatePerHr;
+			this.noOfDays = noOfDays;
+			this.maxHrsPerMonth = maxHrsPerMonth;
+		}
+
+		public void setTotalWage(int totalWage) {
+			this.totalWage = totalWage;
+		}
+
+		@Override
+		public String toString(){
+			return "Total Employee Wage for Company " + company + " is " + totalWage;
+		}
+}
--- EmployeeWage.java
+++ EmployeeWage.java
@@ -47,28 +47,4 @@ public class EmployeeWage{
                      emp.calcEmpWage();
                 }
 
-         class CompanyEmpWage{
-	
-	        public final String company;
-	        public final int empRatePerHr;
-	        public final int noOfDays;
-	        public final int maxHrsPerMonth;
-	        public int totalWage;
-	
-	        public CompanyEmpWage(String company, int empRatePerHr, int noOfDays, int maxHrsPerMonth) {
-		     this.company = company;
-		     this.empRatePerHr = empRatePerHr;
-		     this.noOfDays =noOfDays;
-		     this.maxHrsPerMonth = maxHrsPerMonth;
-	        }
-	
-	        public void setTotalWage(int totalWage) {
-		      this.totalWage = totalWage;
-	        }
-
-                @Override
-                public String toString(){
-                     return "Total Employee Wage for company " + company +" is " + totalWage;
-                }
-        }
 }
