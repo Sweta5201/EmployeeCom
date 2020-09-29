3ff7d4b9197efe75fca98fdf8fc3f04571c45d95
Sweta5201 <sweta65959@gmail.com>
2020-09-29 11:39:39 +0530

    [sweta kumari] uc1 done

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,12 +1,11 @@
-class EmployeeWage{
-public static void main(String[] args){
-int is_present = 1;
-int check =(int) Math.floor(Math.random() *10) % 2;
-if(check == is_present){
-System.out.println("Employee is present");
-}
-else{
-System.out.println("Employee is absent");
-}
-}
+public class EmployeeWage{
+     public static final int IS_PRESENT = 1;
+     public static void main(String[] args){
+     int check =(int) Math.floor(Math.random() *10) % 2;
+     if(check == IS_PRESENT)
+        System.out.println("Employee is present");
+     else
+        System.out.println("Employee is absent");
+
+    }
 }
