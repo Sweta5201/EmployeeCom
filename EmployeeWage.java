7927bc2f0360ea21dac543df7e8e267f99c6977b
Sweta5201 <sweta65959@gmail.com>
2020-09-29 13:14:10 +0530

    [sweta kumari] uc8 done

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,30 +1,32 @@
 public class EmployeeWage{
-public static final int Part_time = 1;
-public static final int Full_time =2;
-public static final int Rate_per_hour = 20;
-public static final int Working_days = 20;
-public static final int max_hrs_in_month = 100;
-public static void main(String[] args){
-int Emp_hrs = 0;
-int Emp_wage = 0;
-int Total_wage = 0;
-int totalhrs = 0;
-int totalworkingdays=0;
-while(totalhrs <= max_hrs_in_month && totalworkingdays < Working_days)
-{
-totalworkingdays++;
-int check =(int) Math.floor(Math.random() *10) % 3;
-switch(check){
-case Part_time: Emp_hrs = 4;
-                break;
-case Full_time: Emp_hrs = 8;
-                break;
-default: Emp_hrs = 0;
-}
-totalhrs+=Emp_hrs;
-System.out.println("Days : "+totalworkingdays +" Hours : "+Emp_hrs);
-}
-Total_wage = totalhrs * Rate_per_hour;
-System.out.println("Total Employee wage is "+Total_wage);
-}
-}
+		public static final int IS_PART_TIME = 1;
+		public static final int IS_FULL_TIME = 2;
+
+		public static int calcEmpWage(String company, int empRate, int noOfDays, int maxHrs){
+			int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+			while(totalEmpHrs <= maxHrs && totalWorkingDays < noOfDays){
+				totalWorkingDays++;
+				int check = (int) Math.floor(Math.random() * 10) % 3; 
+				switch(check){
+					case IS_PART_TIME:
+						empHrs = 4;
+						break;
+					case IS_FULL_TIME:
+						empHrs = 8;
+						break;
+					default:
+						empHrs = 0;
+				}
+				totalEmpHrs += empHrs;
+				System.out.println("Days: " +  totalWorkingDays + " EmpHours: " + empHrs);
+			}
+			int totalWage = totalEmpHrs * empRate;
+			System.out.println("Total Employee Wage for company "+company+" is " + totalWage);
+			return totalWage;
+		}
+
+		public static void main(String[] args){
+			calcEmpWage("Airtel",20, 20, 100);
+	                calcEmpWage("Jio",20, 2, 10);
+		}
+        }
