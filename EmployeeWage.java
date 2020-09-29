fe46d6b553234bdfc4ff4056bbf5a4f8b68d2a5c
Sweta5201 <sweta65959@gmail.com>
2020-09-29 09:34:24 +0530

    [sweta kumari] uc7 done

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,30 +1,37 @@
 public class EmployeeWage{
-public static final int Part_time = 1;
-public static final int Full_time =2;
-public static final int Rate_per_hour = 20;
-public static final int Working_days = 20;
-public static final int max_hrs_in_month = 100;
-public static void main(String[] args){
-int Emp_hrs = 0;
-int Emp_wage = 0;
-int Total_wage = 0;
-int totalhrs = 0;
-int totalworkingdays=0;
-while(totalhrs <= max_hrs_in_month && totalworkingdays < Working_days)
-{
-totalworkingdays++;
-int check =(int) Math.floor(Math.random() *10) % 3;
-switch(check){
-case Part_time: Emp_hrs = 4;
-                break;
-case Full_time: Emp_hrs = 8;
-                break;
-default: Emp_hrs = 0;
-}
-totalhrs+=Emp_hrs;
-System.out.println("Days : "+totalworkingdays +" Hours : "+Emp_hrs);
-}
-Total_wage = totalhrs * Rate_per_hour;
-System.out.println("Total Employee wage is "+Total_wage);
-}
+	//Constants
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
+	public static int computeEmpWage(){
+		//Variables
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		//Computation
+		while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <NUM_OF_WORKING_DAYS){
+			totalWorkingDays++;
+			//check remainder with 3 because 0,1 or 2 possible
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3; 
+			switch(empCheck){
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				default:
+					empHrs = 0;
+			}
+			totalEmpHrs += empHrs;
+			System.out.println("Days: " +  totalWorkingDays + " EmpHours: " + empHrs);
+		}			
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Total Emp Wage: " + totalEmpWage);
+		return totalEmpWage;
+	}
+
+	public static void main(String[] args){
+			computeEmpWage();
+	}
 }
