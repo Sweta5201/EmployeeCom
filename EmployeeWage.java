db98d8e19993b8078843051c27224e4a0d45c02e
Sweta5201 <sweta65959@gmail.com>
2020-09-29 12:27:41 +0530

    [sweta kumari] uc4 done

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,19 +1,22 @@
 public class EmployeeWage{
-public static final int Part_time = 1;
-public static final int Full_time =2;
-public static final int Rate_per_hour = 20;
-public static void main(String[] args){
-int Emp_hrs = 0;
-int Emp_wage = 0;
-int check =(int) Math.floor(Math.random() *10) % 3;
-switch(check){
-case Part_time: Emp_hrs = 4;
-                break;
-case Full_time: Emp_hrs = 8;
-                break;
-default: Emp_hrs = 0;
-}
-Emp_wage = Emp_hrs * Rate_per_hour;
-System.out.println("Employee wage is "+Emp_wage);
-}
+        public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static void main(String[] args){
+		int empHrs = 0;
+		int empWage = 0;
+		int check = (int) Math.floor(Math.random() * 10) % 3; 
+		switch(check){
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+		}
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+                System.out.println("Employee wage is "+ empWage);
+       }
 }
