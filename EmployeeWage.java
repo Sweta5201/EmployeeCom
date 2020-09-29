3eece79d6ca5e9e666138a00cf0191f5f4de42e8
Sweta5201 <sweta65959@gmail.com>
2020-09-29 13:35:15 +0530

    [sweta kumari] uc9 done

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,10 +1,22 @@
 public class EmployeeWage{
 		public static final int IS_PART_TIME = 1;
 		public static final int IS_FULL_TIME = 2;
-
-		public static int calcEmpWage(String company, int empRate, int noOfDays, int maxHrs){
+               
+                private final String company;
+	        private final int empRatePerHr;
+	        private final int noOfDays;
+	        private final int maxHrsPerMonth;
+	        private int totalWage;
+		
+                public EmployeeWage(String company, int empRatePerHr, int noOfDays, int maxHrsPerMonth){
+                      this.company = company;
+                      this.empRatePerHr = empRatePerHr;
+                      this.noOfDays = noOfDays;
+                      this.maxHrsPerMonth = maxHrsPerMonth;
+                }
+                public void calcEmpWage(){
 			int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-			while(totalEmpHrs <= maxHrs && totalWorkingDays < noOfDays){
+			while(totalEmpHrs <= maxHrsPerMonth && totalWorkingDays < noOfDays){
 				totalWorkingDays++;
 				int check = (int) Math.floor(Math.random() * 10) % 3; 
 				switch(check){
@@ -20,13 +32,20 @@ public class EmployeeWage{
 				totalEmpHrs += empHrs;
 				System.out.println("Days: " +  totalWorkingDays + " EmpHours: " + empHrs);
 			}
-			int totalWage = totalEmpHrs * empRate;
-			System.out.println("Total Employee Wage for company "+company+" is " + totalWage);
-			return totalWage;
+		        totalWage = totalEmpHrs * empRatePerHr;
 		}
+               
+                @Override
+                public String toString(){
+                     return "Total Employee Wage for company " + company +" is " + totalWage;
+                }
 
 		public static void main(String[] args){
-			calcEmpWage("Airtel",20, 20, 100);
-	                calcEmpWage("Jio",20, 2, 10);
+                     EmployeeWage airtel = new EmployeeWage("Airtel",20, 20, 100);
+	             EmployeeWage jio = new EmployeeWage("Jio",20, 18, 90);
+                     airtel.calcEmpWage();
+                     System.out.println(airtel);
+                     jio.calcEmpWage();
+                     System.out.println(jio);
 		}
         }
